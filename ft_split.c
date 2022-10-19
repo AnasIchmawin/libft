d286d2fdfbf843620b812bd95936c025ac846324
aichmawi <ichmawinanas7@gmail.com~>
2022-10-19 17:03:28 -0400

    .

--- ft_split.c
+++ ft_split.c
@@ -12,62 +12,63 @@
 
 #include "libft.h"
 
-void    alloc(char **s, size_t count)
+void	alloc(char **s, size_t count)
 {
-    s = (char **)malloc(sizeof(char *) * (count + 2));
-    if (*s == NULL)
-        return (NULL);
+	s = (char **)malloc(sizeof(char *) * (count + 2));
+	if (*s == NULL)
+		return ;
 }
 
-size_t count(char const *s,char c)
+size_t	count(char const *s, char c)
 {
-    size_t  count;
+	size_t	count;
+	int i;
 
-    count = 0;
-    if (s == NULL)
-        return (0);
-    while (s[i])
-    {
-        if(s[i] == c)
-            count++;
-    }
-    return (count);
+	count = 0;
+	i = 0;
+	if (s == NULL)
+		return (0);
+	while (s[i])
+	{
+		if (s[i] == c)
+			count++;
+	}
+	return (count);
 }
 
-char *changec(char const *p, char c)
+char	*changec(char *p, char c)
 {
-    int i;
-    i = 0;
-    if(p == NULL)
-        return (NULL);
-    while(p[i])
-    {
-        if(p[i] == c)
-            p[i] = '\0';
-        i++;
-    }
-    return (p);
+	int	i;
+
+	i = 0;
+	if (p == NULL)
+		return (NULL);
+	while (p[i])
+	{
+		if (p[i] == c)
+			p[i] = '\0';
+		i++;
+	}
+	return (p);
 }
 
-char **ft_split(char const *s, char c)
+char	**ft_split(char const *s, char c)
 {
-    char    **p;
-    char    *tmp;
-    size_t     i;
-    size_t     len;
+	char	**p;
+	char	*tmp;
+	size_t	i;
+	size_t	len;
 
-    i = 0;
-    len = 0;
-    while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
-        i++;
-    alloc(p, count(s, c));
-    tmp = changec(s, c);
-    while (tmp[i])
-    {
-        
-        if(tmp[i] == '\0' && tmp[i + 1] != '\0')
-            i++;
-        i++;
-    }
-    
+	i = 0;
+	len = 0;
+	while (s[i] == 32 || (s[i] >= 9 && s[i] <= 13))
+		i++;
+	alloc(p, count(s, c));
+	tmp = changec(s, c);
+	while (tmp[i])
+	{
+		if (tmp[i] == '\0' && tmp[i + 1] != '\0')
+			i++;
+		i++;
+	}
 }
