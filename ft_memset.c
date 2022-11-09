be1feb8535169beb885476187f50e984acb34c8f
Anas Ichmawin <ichmawinanas7@gmail.com>
2022-11-09 12:55:06 +0100

    .

--- ft_memset.c
+++ ft_memset.c
@@ -14,10 +14,10 @@
 void	*ft_memset(void *b, int c, size_t len)
 {
 	size_t	i;
-	char	*a;
+	unsigned char	*a;
 
 	i = 0;
-	a = (char *)b;
+	a = (unsigned char *)b;
 	while (i < len)
 	{
 		a[i] = c;
--- ft_strjoin.c
+++ ft_strjoin.c
@@ -19,7 +19,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	s = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2)));
+	s = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (s == NULL)
 		return (NULL);
 	i = 0;
