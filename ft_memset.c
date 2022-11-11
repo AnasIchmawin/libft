429b15ec984998ae3722ec2cc1e1db56e523bbfc
Anas Ichmawin <ichmawinanas7@gmail.com>
2022-11-11 10:13:37 +0100

    .

--- ft_memset.c
+++ ft_memset.c
@@ -6,14 +6,14 @@
 /*   By: aichmawi <aichmawi@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/08 10:37:47 by aichmawi          #+#    #+#             */
-/*   Updated: 2022/10/09 12:00:42 by aichmawi         ###   ########.fr       */
+/*   Updated: 2022/11/11 10:12:53 by aichmawi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t	i;
+	size_t			i;
 	unsigned char	*a;
 
 	i = 0;
